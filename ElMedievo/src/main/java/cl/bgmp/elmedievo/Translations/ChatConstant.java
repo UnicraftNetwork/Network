067e5f5c97b36f05cf6fb5335d5805264c221d5d
BuildTools <unconfigured@null.spigotmc.org>
2020-04-06 21:20:21 -0400

    Translate ES_CL updated

--- ElMedievo/src/main/java/cl/bgmp/elmedievo/Translations/ChatConstant.java
+++ ElMedievo/src/main/java/cl/bgmp/elmedievo/Translations/ChatConstant.java
@@ -9,48 +9,30 @@ public enum ChatConstant {
   NO_PLAYER("You must execute this command via server console!"),
 
   // TPA
-  @MissingTranslation
   TPA_SENT("Teleport request successfully sent to {0}."),
   ALREADY_REQUESTING_TPA("You are already requesting to teleport to this player."),
-  @MissingTranslation
   TPA_NO_PENDANT("You do not have any incoming tpa."),
   TPA_ACCEPT("Successfully accepted {0}'s teleport request."),
-  @MissingTranslation
   TPA_ACCEPTED("Your teleport request was accepted by {0}!"),
-  @MissingTranslation
   TPA_DENY("You have denied an incoming teleport request from {0}."),
-  @MissingTranslation
   TPA_DENIED("{0} has denied your teleport request to them."),
-  @MissingTranslation
   TPA_CANCEL("Successfully cancelled outgoing teleport request to {0}."),
-  @MissingTranslation
   TPA_CANCELLED("Incoming teleport request was cancelled by {0}."),
-
-  @MissingTranslation
   TPA_EXPIRED("Your teleport request to {0} has expired."),
-  @MissingTranslation
   TPA_RECEIVED(
       "{0} &ais requesting to teleport to you.-|&a➥ Use &b/tpaccept &aor &c/tpdeny&a.-|&aThis request will expire in &4120 &aseconds."),
   @MissingTranslation
   RELOADED_FURNACE_RECIPES("Successfully reloaded all furnace recipes."),
-  @MissingTranslation
   TPA_CANCELLED_ALL("Cancelled all outgoing teleport requests."),
-  @MissingTranslation
   NO_TPAS("No Teleport requests to display."),
-  @MissingTranslation
   TPA_MAP("TPA Map"),
-  @MissingTranslation
   ACCEPT_BUTTON("Accept"),
-  @MissingTranslation
   ACCEPT_BUTTON_HOVER("Click to accept"),
 
   // Misc
-  @MissingTranslation
   NUMBER_STRING_EXCEPTION("Expected a number, string received instead."),
-  @MissingTranslation
   UNKNOWN_ERROR("An unknown error has occurred."),
   INVALID_PLAYER("No players matched query."),
-  @MissingTranslation
   SPAWN_TELEPORTED("You have been teleported to the spawn.");
 
   private String string;
--- ElMedievo/src/main/java/cl/bgmp/elmedievo/Translations/Languages/ES_CL.java
+++ ElMedievo/src/main/java/cl/bgmp/elmedievo/Translations/Languages/ES_CL.java
@@ -3,6 +3,7 @@ package cl.bgmp.elmedievo.Translations.Languages;
 import cl.bgmp.elmedievo.Translations.ChatConstant;
 
 public enum ES_CL {
+  // Comandos
   NO_PERMISSION(ChatConstant.NO_PERMISSION, "No tienes permiso mierda!"),
   TOO_FEW_ARGS(ChatConstant.TOO_FEW_ARGS, "Muy pocos argumentos."),
   TOO_MANY_ARGS(ChatConstant.TOO_MANY_ARGS, "Demasiados argumentos."),
@@ -10,9 +11,35 @@ public enum ES_CL {
   NO_PLAYER(
       ChatConstant.NO_PLAYER, "Tienes que ejecutar este comando via la consola del servidor!"),
   INVALID_PLAYER(ChatConstant.INVALID_PLAYER, "Jugador no encontrado."),
+  NUMBER_STRING_EXCEPTION(ChatConstant.NUMBER_STRING_EXCEPTION, "Tienes que esperar un número."),
+  UNKNOWN_ERROR(ChatConstant.UNKNOWN_ERROR, "Ha ocurrido un error desconocido."),
+  SPAWN_TELEPORTED(ChatConstant.SPAWN_TELEPORTED, "Has sido teletransportado al spawn."),
+
+  // TPA
   ALREADY_REQUESTING_TPA(
       ChatConstant.ALREADY_REQUESTING_TPA, "Ya tienes una solicitud enviada a ese jugador!."),
-  TPA_ACCEPT(ChatConstant.TPA_ACCEPT, "Aceptaste el tpa de {0}!");
+  TPA_ACCEPT(ChatConstant.TPA_ACCEPT, "Aceptaste el tpa de {0}!"),
+  TPA_SENT(ChatConstant.TPA_SENT, "Solicitud de tpa enviada a {0}"),
+  TPA_NO_PENDANT(ChatConstant.TPA_NO_PENDANT, "No tienes ninguna solicitud de tpa pendiente."),
+  TPA_ACCEPTED(ChatConstant.TPA_ACCEPTED, "Tu solicitud de tpa ha sido aceptada por {0}!"),
+  TPA_DENY(ChatConstant.TPA_DENY, "Has rechazado la solicitud de tpa que te envió {0}."),
+  TPA_DENIED(ChatConstant.TPA_DENIED, "{0} ha rechazado la solicitud de tpa que le enviaste."),
+  TPA_CANCEL(ChatConstant.TPA_CANCEL, "Cancelaste la solicitud de tpa enviada a {0}."),
+  TPA_CANCELLED(ChatConstant.TPA_CANCELLED, "La solicitud de tpa entrante fue cancelada por {0}."),
+  TPA_EXPIRED(ChatConstant.TPA_EXPIRED, "Tu solicitud de tpa enviada a {0} expiró."),
+  TPA_RECEIVED(
+      ChatConstant.TPA_RECEIVED,
+      "{0} &ate envió una solicitud de tpa.-|&a➥ Usa &b/tpaccept para aceptarla &ao &c/tpdeny para rechazarla&a.-|&aEsta solicitud expirará en &4120 &asegundos."),
+  TPA_CANCELLED_ALL(
+      ChatConstant.TPA_CANCELLED_ALL, "Se cancelaron todas las solicitudes de tpa entrantes."),
+  NO_TPAS(ChatConstant.NO_TPAS, "No tienes solicitudes de tpa pendientes."),
+  ACCEPT_BUTTON(ChatConstant.ACCEPT_BUTTON, "Aceptar"),
+  ACCEPT_BUTTON_HOVER(ChatConstant.ACCEPT_BUTTON_HOVER, "Click para aceptar."),
+  TPA_MAP(ChatConstant.TPA_MAP, "Mapa tpa"),
+
+  // Misc
+  RELOADED_FURNACE_RECIPES(
+      ChatConstant.RELOADED_FURNACE_RECIPES, "Se recargaron todas las recetas del horno.");
 
   private ChatConstant chatConstant;
   private String translation;

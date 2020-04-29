3db568b6e7d1ccb1536c25b23c01da1c8e8cc15f
BGM <bgmp00@protonmail.com>
2020-04-29 02:33:17 -0400

    Introduce new internal translations logic in utils
    Progress towards #15

---
+++ Utils-Bukkit/src/main/java/cl/bgmp/utilsbukkit/Translations/TranslatedLanguage.java
@@ -0,0 +1,30 @@
+package cl.bgmp.utilsbukkit.Translations;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class TranslatedLanguage {
+    private String locale;
+    private Properties languagePropertiesFile = new Properties();
+
+    public TranslatedLanguage(String locale) {
+        this.locale = locale;
+        loadFromResources(languagePropertiesFile, this.locale);
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public String get(final String key) {
+        return languagePropertiesFile.getProperty(key);
+    }
+
+    private void loadFromResources(final Properties properties, final String locale) {
+        try {
+            properties.load(this.getClass().getClassLoader().getResourceAsStream("i18n/" + locale + ".properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

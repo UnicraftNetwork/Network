295f41d97d3a6bc3d29d07e1eaf3a25ba3b1ae68
BGM <bgmp00@protonmail.com>
2020-04-30 01:22:54 -0400

    Navigator is now translatable

--- Commons/src/main/java/cl/bgmp/commons/Navigator/Navigator.java
+++ Commons/src/main/java/cl/bgmp/commons/Navigator/Navigator.java
@@ -1,18 +1,28 @@
 package cl.bgmp.commons.Navigator;
 
-import cl.bgmp.utilsbukkit.Chat;
 import cl.bgmp.utilsbukkit.Items.PlayerHeads;
+import cl.bgmp.utilsbukkit.Translations.Translations;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 /** The server navigator players are given when joining */
 public class Navigator {
-  private String title = Chat.colourify("&9&lNavigator&r");
-  private String[] lore = new String[] {Chat.colourify("&7Right-Click to pick a Server!&r")};
-  private ItemStack navigator = PlayerHeads.titledPlayerHeadWithLore("Skyron_Varn", title, lore);
+  public static final String playerHeadOwner = "Skyron_Varn";
+  private ItemStack navigator;
 
-  // Note: if navigator's item happens to be a player head, it must contain a lore for validation
-  // matters!
-  public Navigator() {}
+  public Navigator(final Player player) {
+    final String title =
+        ChatColor.BLUE.toString()
+            + ChatColor.BOLD
+            + Translations.get("module.navigator.title", player)
+            + ChatColor.RESET;
+    final String[] lore =
+        new String[] {
+          ChatColor.GRAY + Translations.get("module.navigator.lore", player) + ChatColor.RESET
+        };
+    this.navigator = PlayerHeads.titledPlayerHeadWithLore(playerHeadOwner, title, lore);
+  }
 
   public ItemStack getItem() {
     return navigator;
